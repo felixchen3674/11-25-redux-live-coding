2631c1d75693f24ff27bd45a59cd5d4b658eb719
Leo <liz1@Leo.local>
2025-01-02 18:16:43 -0800

    update the todolist for the add task function button

--- src/components/todoList/TodoList.jsx
+++ src/components/todoList/TodoList.jsx
@@ -1,10 +1,26 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import React, { useState } from "react";
 import Todo from "./Todo";
+import { addTodo } from "../../redux/reducer/todosReducer";
+
 export default function TodoList() {
   const todos = useSelector((state) => state.todos);
+  const [task, setTask] = useState("");
+  const dispatch = useDispatch();
+  const handleAddTodo = () => {
+    // console.log('handler the add', task)
+    dispatch(addTodo({ id: Date.now(), task: task, completed: false }));
+  };
+
   return (
     <div>
       <h1>Todo List</h1>
+      <input
+        type="text"
+        value={task}
+        onChange={(e) => setTask(e.target.value)}
+      />
+      <button onClick={handleAddTodo}>Add New Todo</button>
       {todos.map((todo) => {
         return <Todo key={todo.id} todo={todo} />;
       })}
